5462130b117ffbe2d4dfc520a5c6556ab6018f40
Евгений Бухарев <uvdrfkostroma@gmail.com>
2018-06-24 00:45:17 +0300

    new1

---
+++ HomeWork2.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+    <title>Домашнее Задание 2</title>
+</head>
+<body>
+<h1>Задание №1</h1>
+<p>Таблица истинности оператора && - логическое "И":</p>
+<table border ='1' cellpadding='5' width='300'>
+    <tr align="center">
+        <td><b>&&</b></td><td><b>false</b></td><td><b>true</b></td>
+    </tr>
+    <tr align="center">
+        <td><b>false</b></td><td>false</td><td>false</td>
+    </tr>
+    <tr align="center">
+        <td><b>true</b></td><td>false</td><td>true</td>
+    </tr>
+</table>
+<p>Таблица истинности оператора | | - логическое "ИЛИ":</p>
+<table  border ='1' cellpadding='5' width='300'>
+    <tr align="center">
+        <td><b>| |</b></td><td><b>false</b></td><td><b>true</b></td>
+    </tr>
+    <tr align="center">
+        <td><b>false</b></td><td>false</td><td>true</td>
+    </tr>
+    <tr align="center">
+        <td><b>true</b></td><td>true</td><td>true</td>
+    </tr>
+</table>
+<p>Таблица истинности оператора XOR - логическое "ИСКЛЮЧАЮЩЕЕ ИЛИ":</p>
+<table border ='1' cellpadding='5' width='300'>
+    <tr align="center">
+        <td><b>XOR</b></td><td><b>false</b></td><td><b>true</b></td>
+    </tr>
+    <tr align="center">
+        <td><b>false</b></td><td>false</td><td>true</td>
+    </tr>
+    <tr align="center">
+        <td><b>true</b></td><td>true</td><td>false</td>
+    </tr>
+</table>
+
+<h1>Задание №2</h1>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
